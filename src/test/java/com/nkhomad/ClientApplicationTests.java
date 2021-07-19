64a74651b9e91753fd5d8f1f65735a18b5de08c0
Daniel Nkhoma <danielnkhoma@gmail.com>
2021-07-19 19:39:09 +0200

    test services

--- src/test/java/com/nkhomad/ClientApplicationTests.java
+++ src/test/java/com/nkhomad/ClientApplicationTests.java
@@ -1,14 +1,31 @@
 package com.nkhomad;
 
+import com.nkhomad.repository.UserRepository;
+import com.nkhomad.rest.UserRestController;
+import com.nkhomad.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClientApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRestController userRestController;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(userService);
+        Assertions.assertNotNull(userRepository);
+        Assertions.assertNotNull(userRestController);
     }
 
 }
